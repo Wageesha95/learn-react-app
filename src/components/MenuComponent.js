5aec3d720d16cc8c670ac9a220901e169df019dc
Thilina Liyanage <37930455+Wageesha95@users.noreply.github.com>
2020-10-17 05:34:55 +0530

    Presentational and container component, moved base to the Main component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,42 +1,31 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import Dishdetail from './Dishdetaicomponent'
 
 class Menu extends Component {
+
     constructor(props) {
         super(props);
-
-        this.state = {
-            selectedDish: null
-        }
     }
-
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish });
-    }
-
-
 
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5  m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width='100%' object src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
                         </CardImgOverlay>
                     </Card >
-                </div>
+                </div >
             );
         });
 
         return (
-            <div className='container'>
+            <div className='container' >
                 <div className="row">
                     {menu}
                 </div>
-                <Dishdetail dish={this.state.selectedDish} />
             </div>
         );
     }
