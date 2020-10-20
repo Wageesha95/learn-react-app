d9c44c4b0fda16ca554eb7c97ae54d270dc0c3ec
Thilina Liyanage <37930455+Wageesha95@users.noreply.github.com>
2020-10-20 21:57:42 +0530

    router added, headers and footers added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import DishDetail from './DishDetailComponent';
-import { DISHES } from '../shared/dishes';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent'
+import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
@@ -11,21 +16,32 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
-            selecteddish: null
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         }
     }
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId });
-    }
+
 
     render() {
+
+        const HomePage = () => {
+            return (
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
+            )
+        }
         return (
             <div>
                 <Header />
-                <Menu dishes={this.state.dishes}
-                    onClick={(dishId) => this.onDishSelect(dishId)} />
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path="/contactus" component={Contact} />
+                    <Redirect to="/home" />
+                </Switch>
                 <Footer />
             </div>
         );
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-
 function RenderMenuItem({ dish, onClick }) {
     return (
-        <Card onClick={() => onClick(dish.id)}>
+        <Card >
             <CardImg width='100%' object src={dish.image} alt={dish.name} />
             <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
